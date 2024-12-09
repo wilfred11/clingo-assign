2adad14d785e1e2cb84f5ab35db0ef89a546d0ba
wilfred11 <wilfredvancasteren@hotmail.com>
2024-12-09 17:52:56 +0100

    updated

--- asp.py
+++ asp.py
@@ -1,24 +1,5 @@
 import clingo
-
 from query import num_col_data
-from clingo.symbol import Function, Number, parse_term
-
-
-
-import sys
-from clingo.application import Application, clingo_main
-
-class ClingoApp(Application):
-    def __init__(self, name):
-        self.program_name = name
-
-    def main(self, ctl, files):
-        for f in files:
-            ctl.load(f)
-        if not files:
-            ctl.load("-")
-        ctl.ground([("base", [])])
-        ctl.solve()
 
 
 def generate_priority_counts():
@@ -37,17 +18,13 @@ def generate_priority_counts():
     for model in control.solve(yield_=True):
         for atom in model.symbols(shown=True):
             f.write(str(atom) + ".\n")
-
     f.close()
 
-def on_model(m):
-    m.context.add_clause([(Function("comb_id", [Number(1)]), False)])
+
 def generate_column_combinations():
     control = clingo.Control()
 
-
     control.add("base", [], "")
-    #control.load("lp-files/fixed-solution/gen-priority-counts-numbers.lp")
     control.load("lp-files/priorities/generated/priority-counts.lp")
     control.load("lp-files/fixed-solution/gen-column-combinations.lp")
     control.load("lp-files/fixed-solution/count-cols-priorities.lp")
@@ -62,31 +39,30 @@ def generate_column_combinations():
 
     with (control.solve(yield_=True) as handle):
         for model in handle:
-            control_model = clingo.Control(["-c", "comb_n="+str(model.number)])
-            control_model.add("base", [], "")
-            #control_model.load("lp-files/generated/solution/columns.lp")
-            #control_model.add("comb(col_priority(X1,S,T),comb_n) :- col_priority(X1,S,T), column(X1).")
-            control_model.add("comb(col_priority(X1,S,T),comb_n) :- col_priority(X1,S,T).")
-            control_model.add("#show comb/2.")
+            model_control = clingo.Control(["-c", "comb_n=" + str(model.number)])
+            model_control.add("base", [], "")
+            model_control.add("comb(col_priority(X1,S,T),comb_n) :- col_priority(X1,S,T).")
+            model_control.add("#show comb/2.")
             for atom in model.symbols(shown=True):
-                control_model.add(str(atom)+".")
+                model_control.add(str(atom) + ".")
 
-            control_model.ground([("base", [])])
-            control_model.configuration.solve.models = 0
-            with control_model.solve(yield_=True) as modelhandle:
-                for model1 in modelhandle:
-                    for atom in model1.symbols(shown=True):
-                        f.write(str(atom)+".\n")
-            count=count+1
+            model_control.ground([("base", [])])
+            model_control.configuration.solve.models = 0
+            with model_control.solve(yield_=True) as modelhandle:
+                for model_ in modelhandle:
+                    for atom in model_.symbols(shown=True):
+                        f.write(str(atom) + ".\n")
+            count = count + 1
 
     for y in range(1, count):
         comb_str += "comb(" + str(y) + ")"
-        if y +1== count:
+        if y + 1 == count:
             comb_str += "."
         else:
             comb_str += "|"
     f.write(comb_str)
     f.close()
+
 
 def generate_k_anonym_data():
     control = clingo.Control(["--opt-mode=optN", "-c", "k=4"])
@@ -94,19 +70,19 @@ def generate_k_anonym_data():
     control.load("lp-files/generated/data/data.lp")
     control.load("lp-files/generated/solution/col-combs.lp")
 
-    del_c_str = ""
+    del_c_str = "del_c(0)|"
     for h in range(1, num_col_data() - 2):
         if h != num_col_data():
             del_c_str += "del_c(" + str(h) + ")"
             del_c_str += "|"
-    del_c_str=del_c_str[:-1]
-    del_c_str+="."
+    del_c_str = del_c_str[:-1]
+    del_c_str += "."
     print(del_c_str)
     control.add(del_c_str)
 
     control.load("lp-files/generated/solution/columns.lp")
-    control.load("lp-files/generated/solution/priority-distr.lp")
-    control.load("lp-files/fixed-solution/gen-asp-data-files.lp")
+    control.load("lp-files/generated/solution/priority-deletion-rank.lp")
+    control.load("lp-files/fixed-solution/search-constraints.lp")
     control.load("lp-files/generated/solution/col-specific-hide.lp")
     control.load("lp-files/generated/solution/values.lp")
     control.ground([("base", [])])
@@ -123,7 +99,7 @@ def generate_k_anonym_data():
 
 
 def generate_csv_from_asp():
-    ids=[]
+    ids = []
     control = clingo.Control()
     control.add("base", [], "")
     control.load("lp-files/generated/data-to-be-exported/data-3.lp")
@@ -152,17 +128,18 @@ def generate_csv_from_asp():
             f.write("\n")
     f.close()
 
-def add_count_priority():
+
+def add_deletion_rank():
     control = clingo.Control()
     control.add("base", [], "")
-    control.load("lp-files/fixed-solution/add-count-priority-numbers.lp")
+    control.load("lp-files/fixed-solution/add-deletion-rank-priority-numbers.lp")
     control.load("lp-files/priorities/col-priorities.lp")
     control.load("lp-files/fixed-solution/count-cols-priorities.lp")
     control.load("lp-files/priorities/generated/priority-counts.lp")
     control.ground([("base", [])])
     control.configuration.solve.models = 0
 
-    f = open("lp-files/generated/solution/priority-distr.lp", "w")
+    f = open("lp-files/generated/solution/priority-deletion-rank.lp", "w")
     for model in control.solve(yield_=True):
         for atom in model.symbols(shown=True):
             f.write(str(atom) + ".\n")
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import clingo
 
 from asp import generate_column_combinations, \
-    generate_priority_counts, add_count_priority, generate_k_anonym_data, generate_csv_from_asp
+    generate_priority_counts, add_deletion_rank, generate_k_anonym_data, generate_csv_from_asp
 from query import read_data, check, quasi_identifiers, create_initial_lp_data, quasi_identifiers1, create_string_data
 
-do = 7
+do = 4
 random_state = 10
 
 
@@ -21,7 +21,7 @@ if do == 4:
     generate_column_combinations()
 
 if do == 5:
-    add_count_priority()
+    add_deletion_rank()
 
 if do == 6:
     generate_k_anonym_data()
